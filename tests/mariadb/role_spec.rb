10d2fdfd69910bf6913987a7fb63cebe4892109d
sven klemmer <svenklemmer@aol.com>
2018-05-12 11:42:10 +0200

    adjust socket paths

--- tests/mariadb/role_spec.rb
+++ tests/mariadb/role_spec.rb
@@ -3,108 +3,139 @@
 ##
 # general
 ##
-describe service('docker') do
-  it {should be_installed}
-  it {should be_enabled}
-  it {should be_running}
-end
+control "general" do
+  impact 0.7
+  title "Test General Docker Config"
 
-describe docker_image('mariadb:latest') do
-  it { should exist }
-  its('repo') { should eq 'mariadb'}
-  its('tag') { should eq 'latest'}
+  describe service('docker') do
+    it {should be_installed}
+    it {should be_enabled}
+    it {should be_running}
+  end
+
+  describe docker_image('mariadb:latest') do
+    it { should exist }
+    its('repo') { should eq 'mariadb'}
+    its('tag') { should eq 'latest'}
+  end
 end
 
 ##
 # with port
 ##
-describe docker_container('maria-with-port') do
-  it { should exist }
-  it { should be_running }
-  its('image') { should eq 'mariadb:latest' }
-  its('repo') { should eq 'mariadb' }
-  its('tag') { should eq 'latest' }
-  its('ports') { should eq ["3306:3306"] }
-end
+control "with-port" do
+  impact 0.7
+  title "Test mariadb with open ports"
 
-describe mysql_conf('/etc/mysql/with_port.cnf') do
-  its('port') { should eq '3306' }
-end
-
-describe file('/etc/mysql/with_port.cnf') do
-  it { should exist }
-  its('type') { should eq :file }
-end
-
-describe directory('/data/var/lib/mysql-with-port') do
-  it { should exist }
-end
-
-describe port(3306) do
-  its('protocols') { should include 'tcp' }
+  # container status
+  describe docker_container('maria-with-port') do
+    it { should exist }
+    it { should be_running }
+    its('image') { should eq 'mariadb:latest' }
+    its('repo') { should eq 'mariadb' }
+    its('tag') { should eq 'latest' }
+    its('ports') { should eq "0.0.0.0:3306->3306/tcp" }
+  end
+  # conf file existence
+  describe file('/etc/mysql/with_port.cnf') do
+    it { should exist }
+    its('type') { should eq :file }
+  end
+  # mysql-conf check
+  describe mysql_conf('/etc/mysql/with_port.cnf') do
+    its('client-server.port') { should eq '3306' }
+  end
+  # port existence
+  describe port(3306) do
+    it { should be_listening }
+    its('protocols') { should include 'tcp' }
+  end
+  # data directory existence
+  describe directory('/data/var/lib/mysql-with-port') do
+    it { should exist }
+  end
 end
 
 ##
 # with socket
 ##
-describe docker_container('maria-with-socket') do
-  it { should exist }
-  it { should be_running }
-  its('image') { should eq 'mariadb:latest' }
-  its('repo') { should eq 'mariadb' }
-  its('tag') { should eq 'latest' }
-  its('ports') { should eq [] }
-end
+control "with-socket" do
+  impact 0.7
+  title "Test mariadb with shared socket"
 
-describe mysql_conf('/etc/mysql/with_socket.cnf') do
-  its('socket') { should eq '/var/run/mysqld/mysql_socket.sock' }
-end
-
-describe file('/etc/mysql/with_socket.cnf') do
-  it { should exist }
-  its('type') { should eq :file }
-end
-
-describe file('/var/run/mysqld/mysql_socket.sock') do
-  it { should exist }
-  its('type') { should eq :socket }
-end
-
-describe directory('/data/var/lib/mysql-with-socket') do
-  it { should exist }
+  # container status
+  describe docker_container('maria-with-socket') do
+    it { should exist }
+    it { should be_running }
+    its('image') { should eq 'mariadb:latest' }
+    its('repo') { should eq 'mariadb' }
+    its('tag') { should eq 'latest' }
+    its('ports') { should eq "" }
+  end
+  # conf file existence
+  describe file('/etc/mysql/with_socket.cnf') do
+    it { should exist }
+    its('type') { should eq :file }
+  end
+  # mysql-conf check
+  describe mysql_conf('/etc/mysql/with_socket.cnf') do
+    its('client-server.socket') { should eq '/var/run/mysqld/socket/mysql.sock' }
+  end
+  # socket existence
+  describe directory('/var/run/mysqld/mysql_socket/') do
+    it { should exist }
+  end
+  describe file('/var/run/mysqld/mysql_socket/mysql.sock') do
+    it { should exist }
+    its('type') { should eq :socket }
+  end
+  # data directory existence
+  describe directory('/data/var/lib/mysql-with-socket') do
+    it { should exist }
+  end
 end
 
 ##
 # with socket and port
 ##
-describe docker_container('mysql') do
-  it { should exist }
-  it { should be_running }
-  its('image') { should eq 'mariadb:latest' }
-  its('repo') { should eq 'mariadb' }
-  its('tag') { should eq 'latest' }
-  its('ports') { should eq ["3307:3306"] }
-end
+control "with-socket-port" do
+  impact 0.7
+  title "Test mariadb with open ports and shared socket"
 
-describe mysql_conf('/etc/mysql/my.cnf') do
-  its('port') { should eq '3306' }
-  its('socket') { should eq '/var/run/mysqld/mysql.sock' }
-end
-
-describe file('/etc/mysql/my.cnf') do
-  it { should exist }
-  its('type') { should eq :file }
-end
-
-describe directory('/data/var/lib/mysql') do
-  it { should exist }
-end
-
-describe file('/var/run/mysqld/mysql.sock') do
-  it { should exist }
-  its('type') { should eq :socket }
-end
-
-describe port(3307) do
-  its('protocols') { should include 'tcp' }
+  # container status
+  describe docker_container('mysql') do
+    it { should exist }
+    it { should be_running }
+    its('image') { should eq 'mariadb:latest' }
+    its('repo') { should eq 'mariadb' }
+    its('tag') { should eq 'latest' }
+    its('ports') { should eq "0.0.0.0:3307->3306/tcp" }
+  end
+  # conf file existence
+  describe file('/etc/mysql/my.cnf') do
+    it { should exist }
+    its('type') { should eq :file }
+  end
+  # mysql-conf-check
+  describe mysql_conf('/etc/mysql/my.cnf') do
+    its('client-server.port') { should eq '3306' }
+    its('client-server.socket') { should eq '/var/run/mysqld/socket/mysql.sock' }
+  end
+  # socket existence
+  describe directory('/var/run/mysqld/socket/') do
+    it { should exist }
+  end
+  describe file('/var/run/mysqld/socket/mysql.sock') do
+    it { should exist }
+    its('type') { should eq :socket }
+  end
+  # port existence
+  describe port(3307) do
+    it { should be_listening }
+    its('protocols') { should include 'tcp' }
+  end
+  # data directory existence
+  describe directory('/data/var/lib/mysql') do
+    it { should exist }
+  end
 end
